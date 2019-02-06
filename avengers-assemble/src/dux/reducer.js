ac7cc84ddf94c6554920d0e9df9db718bdd4025b
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2019-02-06 16:04:23 -0700

    Filled out reducer and action creators

--- avengers-assemble/src/dux/reducer.js
+++ avengers-assemble/src/dux/reducer.js
@@ -1,5 +1,33 @@
+let initialState = {
+    allAvengers: [],
+    teamCap: [],
+    teamStark: []
+}
 
+const ADD_TO_CAP = 'ADD_TO_CAP';
+const ADD_TO_STARK = 'ADD_TO_STARK'
 
-export default function reducer(){
-    
+export default function reducer(state=initialState, action){
+    switch(action.type){
+        case ADD_TO_CAP:
+            return Object.assign({}, state, {teamCap: [...state.teamCap, action.payload]});
+        case ADD_TO_STARK:
+            return Object.assign({}, state, {teamStark: [...state.teamStark, action.payload]})
+        default:
+            return state;
+    }
+}
+
+export function addToCap(avenger){
+    return {
+        type: ADD_TO_CAP,
+        payload: avenger
+    }
+}
+
+export function addToStark(avenger){
+    return {
+        type: ADD_TO_STARK,
+        payload: avenger
+    }
 }
